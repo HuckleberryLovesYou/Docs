8e554f2d3cdf4bca2d9296c7afcf17bbc6eb15c2
Tim Matheis <tim.matheis@outlook.com>
2026-02-03 22:41:20 +0100

    fix: stop assets from showing up in the explorer.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,5 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
-import * as Component from "./quartz/components"
+import { PageLayout, SharedLayout } from "./quartz/cfg";
+import * as Component from "./quartz/components";
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -12,7 +12,7 @@ export const sharedPageComponents: SharedLayout = {
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
-}
+};
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
@@ -43,43 +43,43 @@ export const defaultContentPageLayout: PageLayout = {
       folderClickBehavior: "collapse",
       sortFn: (a: any, b: any) => {
         const nameOrderMap: Record<string, number> = {
-          "Home": 0,
-          "Leistungsnachweise": 100,
-          "LICENSE": 200,
-          "CONTRIBUTING": 300,
-        }
- 
-        let orderA = 500
-        let orderB = 500
- 
+          Home: 0,
+          Leistungsnachweise: 100,
+          LICENSE: 200,
+          CONTRIBUTING: 300,
+        };
+
+        let orderA = 500;
+        let orderB = 500;
+
         if (a.file && a.file.frontmatter && a.file.frontmatter.order) {
-          orderA = a.file.frontmatter.order
+          orderA = a.file.frontmatter.order;
         } else if (a.displayName in nameOrderMap) {
-          orderA = nameOrderMap[a.displayName]
+          orderA = nameOrderMap[a.displayName];
         }
- 
+
         if (b.file && b.file.frontmatter && b.file.frontmatter.order) {
-          orderB = b.file.frontmatter.order
+          orderB = b.file.frontmatter.order;
         } else if (b.displayName in nameOrderMap) {
-          orderB = nameOrderMap[b.displayName]
+          orderB = nameOrderMap[b.displayName];
         }
- 
+
         if (orderA !== orderB) {
-          return orderA - orderB
+          return orderA - orderB;
         }
- 
+
         if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
           // Sort alphabetically
           return a.displayName.localeCompare(b.displayName, undefined, {
             numeric: true,
             sensitivity: "base",
-          })
+          });
         }
- 
+
         if (!a.isFolder && b.isFolder) {
-          return 1
+          return 1;
         } else {
-          return -1
+          return -1;
         }
       },
     }),
@@ -88,7 +88,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
-}
+};
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
@@ -111,4 +111,4 @@ export const defaultListPageLayout: PageLayout = {
     }),
   ],
   right: [],
-}
+};
